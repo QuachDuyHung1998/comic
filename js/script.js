f710a04124f45324fe1ccc256d4fd36d97f510dc
QuachDuyHung1998 <quachduyhung.1998@gmail.com>
2020-12-20 22:08:02 +0700

    up

--- js/script.js
+++ js/script.js
@@ -19,6 +19,8 @@ window.onscroll = function () {
 };
 document.getElementById("back-to-top").addEventListener("click", function () {
   window.scrollTo(0, 0);
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
 });
 
 // user action desktop
